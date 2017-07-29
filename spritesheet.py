c2e742c0b42d5dc0e9279abb22d930528b4f3728
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2017-07-29 15:31:32 -0400

    Add documentation to get_spritesheet

--- spritesheet.py
+++ spritesheet.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.2"
+__version__ = "0.1.0"
 __author__ = "Akhier Dragonheart"
 __license__ = "MIT"
 
@@ -8,6 +8,39 @@ import pygame
 
 
 def get_spritesheet(jsonpath):
+    '''Get a spritesheet though data stored in a json file
+
+    This function expects all sprites to be the same size. Along with that it
+    needs your json file to have a 'file' with the location of the image
+    to get the sprites from, 'sprite_width' and 'sprite_height' to define the
+    size of the sprites you want, 'colorkey' which should either be the color
+    you want keyed, -1 which will grab the top left pixels color to use, or
+    false if you don't want a color keyed, and finally 'sprites' which contains
+    each row of your tileset in order which themselves need to contain 'id'
+    which is the name of the sprite and in the order they appear in the row
+    For an example of this:
+    {
+    "file": "examplesheet.png",
+    "colorkey": false,
+    "sprite_width":50,
+    "sprite_height":50,
+    "sprites": [
+            {
+                "id": "yellow",
+                "id": "blue",
+                "id": "purple"
+            },
+            {
+                "id": "green",
+                "id": "red"
+            }
+        ]
+    }
+
+    :param jsonpath: This is were the json file detailing your
+    spritesheet is located
+    :return: Dictionary containing pairs of (sprite_name, sprite_image)
+    '''
     with open(jsonpath) as json_data:
         sheet_data = json.load(json_data)
     try:
