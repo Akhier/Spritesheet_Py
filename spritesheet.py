035a7d1d04b847d38bdf884cd990e3edbab23cc3
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2017-07-29 15:03:03 -0400

    Add try to load image into get_spritesheet

--- spritesheet.py
+++ spritesheet.py
@@ -4,8 +4,13 @@ __license__ = "MIT"
 
 
 import json
+import pygame
 
 
 def get_spritesheet(jsonpath):
     with open(jsonpath) as jsondata:
         sheetdata = json.load(jsondata)
+    try:
+        fullimage = pygame.image.load(sheetdata['file']).convert()
+    except FileNotFoundError:
+        print(sheetdata['file'] + " not found")
