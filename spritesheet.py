fc6c8fb0f2c85d01130cf85d52926d2cf4b45b57
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2017-07-29 14:57:11 -0400

    Add get_spritesheet with json loading code

--- spritesheet.py
+++ spritesheet.py
@@ -1,3 +1,11 @@
 __version__ = "0.0.1"
 __author__ = "Akhier Dragonheart"
 __license__ = "MIT"
+
+
+import json
+
+
+def get_spritesheet(jsonpath):
+    with open(jsonpath) as jsondata:
+        sheetdata = json.load(jsondata)
